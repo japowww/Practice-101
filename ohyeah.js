0310c046cdccf448190beab5d3aa8116255a6e42
japowww <salesjaspherjules@gmail.com>
2024-09-18 08:44:55 +0800

    9/18/24

---
+++ ohyeah.js
@@ -0,0 +1,11 @@
+function mirrorText(){
+var inputText = document.getElementById('inputText');
+var outputText = document.getElementById('outputText');
+outputText.textContent = inputText.value;
+}
+
+function displayAlert(){
+    var inputText = document.getElementById('inputText').value;
+
+
+}
